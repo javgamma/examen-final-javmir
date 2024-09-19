3c8becfa51858465853f5389c16ba0154c96c291
Javmir <167805975+javgamma@users.noreply.github.com>
2024-09-19 23:14:34 +0200

    card generator completed

---
+++ src/app/cardgenerator/page.jsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+import Cardgenerator from './Cardgenerator'
+
+const page = () => {
+  return (
+    <div>
+      <Cardgenerator/>
+    </div>
+  )
+}
+
+export default page
--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -18,7 +18,7 @@ const Sidebar = () => {
 
         <ul className="mt-6 flex flex-col ">
           <Link
-            href="#"
+            href="cardgenerator"
             className="flex gap-3 rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700 "
           >
             <LucideLayoutTemplate size={20} />
