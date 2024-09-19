484c86357132d8065a416a3502429aff0bd496e0
Javmir <167805975+javgamma@users.noreply.github.com>
2024-09-19 21:03:50 +0200

    cards galery created

---
+++ src/app/cardsgalery/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const cardsgalery = () => {
+  return (
+    <div>
+      hoplaaa
+    </div>
+  )
+}
+
+export default  cardsgalery
--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -34,7 +34,7 @@ const Sidebar = () => {
           </Link>
 
           <Link
-            href="drilinginfo"
+            href="/drilinginfo"
             className="flex gap-2 rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
           >
             <BadgeCheck />
@@ -42,7 +42,7 @@ const Sidebar = () => {
           </Link>
 
           <Link
-            href="#"
+            href="/cardsgalery"
             className="flex gap-2 rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
           >
             <Building2 />
