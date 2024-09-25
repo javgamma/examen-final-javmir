c01de28f1e46d28a65b2bc01eb9d34d81932b889
Javmir <167805975+javgamma@users.noreply.github.com>
2024-09-26 00:59:46 +0200

    card section deleted, skeleton added

--- src/app/cardsgallery/GallerySq.jsx
+++ src/app/cardsgallery/GallerySq.jsx
@@ -3,6 +3,17 @@ import React from "react";
 import CardCatalogue from "./CardCatalogue";
 import { useQuery } from "@tanstack/react-query";
 
+const SkeletonCard = () => (
+  <div className="flex flex-col items-center justify-center text-center gap-6 w-64 h-[391px] m-2 border rounded-lg hover:shadow-xl border-solid border-gray-300 animate-pulse">
+    <div className="w-32 h-48 bg-gray-400 rounded-xl"></div>
+    <div className="flex flex-col gap-3 ">
+      <div className="h-4 bg-gray-300 rounded w-[90px] flex"></div>
+      <div className="h-4 bg-gray-200 rounded w-[40px] flex justify-center"></div>
+    </div>
+    <div className="h-10 bg-gray-400 rounded w-[80px]"></div>
+  </div>
+);
+
 const GallerySq = () => {
   const { isLoading, error, data } = useQuery({
     queryKey: ["repoData"],
@@ -14,21 +25,30 @@ const GallerySq = () => {
     },
   });
 
-  if (isLoading) return "Loading...";
+  if (isLoading) {
+    return (
+      <div className="ml-[320px] p-4">
+        <h2 className="text-[#2a5b45] text-2xl font-semibold text-center mb-6">
+          Últimos origenes
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+          {Array(8).fill(0).map((_, index) => (
+            <SkeletonCard key={index} />
+          ))}
+        </div>
+      </div>
+    );
+  }
 
-  if (error) return "An error has occurred: " + error.message;
+  if (error) return "Ha ocurrido un error: " + error.message;
   console.log(data);
 
   return (
     <div className="ml-[320px] p-4">
-      {" "}
- 
-      <h2 className="text-[#2a5b45] text-2xl font-semibold text-center mb-6">
+     <h2 className="text-[#2a5b45] text-2xl font-semibold text-center mb-6">
         Últimos origenes
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 ">
-        {" "}
-        
         {data.map((coffee) => (
           <CardCatalogue
             key={coffee._id}
